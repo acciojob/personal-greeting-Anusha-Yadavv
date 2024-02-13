4e53b570f7607495192121d808915b0f6656499a
Anusha <143824335+Anusha-Yadavv@users.noreply.github.com>
2024-02-13 23:25:07 +0530

    solution

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,20 @@
 
 import React from "react";
 import './../styles/App.css';
+import React, {useState} from "react";
 
 const App = () => {
+  const [inputValue,setInputValue]=useState("");
+  const onChangeText=(event)=>{
+    setInputValue(event.target.value);
+  }
   return (
     <div>
-        {/* Do not remove the main div */}
+        <form>
+          <label>Enter your name:</label>
+          <input onChange={onChangeText} type="text"/>
+        </form>
+        <p>{`Hello ${inputValue}!`}</p>
     </div>
   )
 }
